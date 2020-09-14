22539f2500edad5272f0a6d85ff3e303007cc337
Pedro Santos <pedrobsantos98@outlook.com>
2020-09-14 18:10:31 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dspesquisa.repository;
+
+import com.devsuperior.dspesquisa.entities.Genre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+}
